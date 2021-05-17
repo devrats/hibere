9985927afee3cfe3beeca11c0c6a9fe7b2437d35
Sharma Ji <devvrat.sharm_cs19@gla.ac.in>
2021-05-17 09:58:11 +0530

    completed till cache in hql queries.

---
+++ src/main/java/com/example/hql/Main.java
@@ -0,0 +1,35 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devvrat Sharma (devrats)
+ *   Date: 5/17/2021
+ *   Time: 9:45 AM
+ *   File: Main.java
+ */
+
+package com.example.hql;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.Query;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("from Student ");
+        Query query1 = session.createQuery("update Student set name = 'devvrat' where id = 7");
+        query1.executeUpdate();
+        Query query2 = session.createQuery("delete from Student where id = :x");
+        query2.setParameter("x",1);
+        query2.executeUpdate();
+        System.out.println(query.getResultList());
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
+}
